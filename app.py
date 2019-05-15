5663da37182a43354d5867f6c6cadccf9e2e7f03
Ian Keith <ian@ianhkeith.com>
2019-05-15 16:34:08 -0500

    Search by daterange and search for all matching names functions are working.

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ class Log(Model):
      employee_name = CharField(max_length=255)
      task_title = CharField(max_length=255)
      date = DateTimeField(default=datetime.datetime.now)
-     time_spent = DateTimeField()  # fill in time spent in minutes
-     general_notes = TextField()
+     time_spent = IntegerField(default=0)
+     general_notes = TextField(default=None)
 
      class Meta:
          database = db
@@ -36,31 +36,121 @@ def initialize():
     db.create_tables([Log], safe=True)
 
 
-def add_entry():
+def add_log():
     """Allows a user to add a work log entry"""
     print("Log Entry Form")
 
+    save = 'n'
+    name = input("What is your name? ")
+    title = input("What is the Task Title?  ")
+    while save == 'n':
+        try:
+            spent_time = int(input("How much time did it take? Enter minutes in numbers only\n>>  "))
+            print("Add your notes here. Press ctl+D when done.")
+            notes = sys.stdin.read().strip()
+            save = input("Would you like to save this log? Y/n  ")
+
+            if save.lower().strip() == 'y':
+                Log.create(
+                    employee_name=name,
+                    task_title=title,
+                    time_spent=spent_time,
+                    general_notes=notes
+                )
+                print("Saved successfully!")
+            else:
+                save = 'not saving pal'
+        except ValueError:
+            print("Please enter minutes as a number only. Ex. 42")
+            continue
+
+
+def edit_log():
+    """Allows a user to edit a log entry"""
     pass
 
 
-def edit_entry():
-    """Allows a user to edit an entry"""
-    pass
+def remove_log(entry):
+    """Allows a user to remove a log entry"""
+    if input("Are you sure?  y/N  ").lower() == 'y':
+        entry.delete_instance()
+        print("Entry deleted!")
 
 
-def remove_entry():
-    """Allows a user to remove an entry"""
-    pass
-
-
-def find_entries():
-    """Allows a user to find entries based on various search criteria"""
+def view_logs(start_date=None, end_date=None):
+    """View all logs"""
     # list all for particular employee
     # list all for a date or search
     # list all for date range
     # list all people if they share a name and allow user to choose
     # display entries one at a time with the ability to page through (previous/next/back)
-    pass
+    logs = Log.select().order_by(Log.date.desc())
+
+    if start_date and end_date:
+        logs = Log.select().order_by(Log.date)
+        logs = logs.where(Log.date.between(start_date, end_date))
+
+    for log in logs:
+        clear_screen()
+        date = log.date.strftime('%A %B %d, %Y %I:%M%p')
+        print("=" * len(date))
+        print(date)
+        print("Name: {}".format(log.employee_name))
+        print("Task Title: {}".format(log.task_title))
+        print("Minutes Worked: {}".format(log.time_spent))
+        print("Notes: {}".format(log.general_notes))
+        print('=' * len(date))
+        print()
+        print("N) for next log entry")
+        print("d) to delete log entry")
+        print('q) return to main menu')
+
+        next_action = input('\nAction: [Ndq]  ').lower().strip()
+        if next_action == 'q':
+            break
+        elif next_action == 'd':
+            remove_log(log)
+
+
+def search_by_name():
+    """Search for logs by employee name"""
+    # view_logs(input("Enter Employee Name: ").lower())
+    logs = Log.select().order_by(Log.date.desc())
+    search_name = input("Enter Employee Name: ").lower()
+    logs = logs.where(Log.employee_name.contains(search_name))
+    print("Matches:\n")
+    for log in logs:
+        print(log.employee_name)
+    input("\nPress enter to return to menu.")
+
+
+def search_by_date():
+    """Search for logs by date range: MM/DD/YYYY"""
+    # logs = Log.select().order_by(Log.date.desc())
+    start_date = datetime.datetime.strptime(input("Enter Start Date MM/DD/YY: "), "%m/%d/%Y")
+    end_date = datetime.datetime.strptime(input("Enter End Date MM/DD/YY: "), "%m/%d/%Y")
+    view_logs(start_date, end_date)
+    # logs = logs.where(Log.date >= start_date & Log.date <= end_date)
+    # for log in logs:
+    #     clear_screen()
+    #     date = log.date.strftime('%A %B %d, %Y %I:%M%p')
+    #     print("=" * len(date))
+    #     print(date)
+    #     print("Name: {}".format(log.employee_name))
+    #     print("Task Title: {}".format(log.task_title))
+    #     print("Minutes Worked: {}".format(log.time_spent))
+    #     print("Notes: {}".format(log.general_notes))
+    #     print('=' * len(date))
+    #     print()
+    #     print("N) for next log entry")
+    #     print("d) to delete log entry")
+    #     print('q) return to main menu')
+
+    #     next_action = input('\nAction: [Ndq]  ').lower().strip()
+    #     if next_action == 'q':
+    #         break
+    #     elif next_action == 'd':
+    #         remove_log(log)
 
 
 def print_entry():
@@ -71,9 +161,17 @@ def print_entry():
 
 
 menu = OrderedDict([
-    ("1", add_entry),
-    ("2", find_entries),
+    ("1", add_log),
+    ("2", view_logs),
+    ("3", search_by_name),
+    ("4", search_by_date)
 ])
+
+sub_menu = OrderedDict([
+    ("e", edit_log),
+    ("d", remove_log),
+])
+
 
 def menu_loop():
     """Display a menu to the user at program start"""
